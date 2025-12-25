d6f3cb6bf07038b6f3834b62b38a77a6f74ebcc4
shivampathak2812 <pathakshivam3738@gmail.com>
2025-12-25 12:49:27 +0530

    Add database Schema  for pizza_sales

---
+++ Schema.sql
@@ -0,0 +1,7 @@
+-- Database=pizza_sales
+-- Generated from exixting table
+use pizza_sales;
+show create table orders;
+show create table order_details;
+show create table pizza_types;
+show create table pizzas;
